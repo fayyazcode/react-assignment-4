4c1b6a22b99bafb2ad986297868b12b271beffa2
Fayyaz Alam <fayazalam.98@gmail.com>
2021-09-13 10:47:56 -0700

    style added

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ const data = ['We ', 'are ', 'United'] //Show these in seperate tags
 function Hello() {
   return (
     <div >
-      <p> {name}</p>
+      <p className="card-body text-primary bg-light"> {name}</p>
     </div>
   );
 }
 function ObjProp() {
   return (
     <>
-      <p>{obj.name}</p>
+      <p className="card-body text-primary bg-light">{obj.name}</p>
     </>
   );
 }
@@ -25,55 +25,59 @@ function ObjProp() {
 function ArrayProp() {
   return (
     <>
-      <span>
-        {data[0]}
-      </span>
-      <span>
-        {data[1]}
-      </span>
-      <span>
-        {data[2]}
-      </span>
+      <div className="card-body">
+
+        <span className=" text-primary bg-light">
+          {data[0]}
+        </span>
+        <span className=" text-primary bg-light">
+          {data[1]}
+        </span>
+        <span className=" text-primary bg-light">
+          {data[2]}
+        </span>
+      </div>
 
     </>);
 }
 
 const list = [{ name: "Hello World 1" }, { name: "Hello World 2" }, { name: "Hello World 3" }] //Show these in seperate tags
-function ArrOfObj (){
-  const listItem =list.map((data,id)=>
-  <li key={id}>{data.name}</li>)
-  return(
-    <ul>{listItem}</ul>
-    );
-  }
-  
-  
-  const complex = [{ company: 'XYZ', jobs: ['Javascript', 'React'] }, { company: 'ABC', jobs: ['AngularJs', 'Ionic'] }]
-  function ComplexArrObj (){
-    const compObj=complex.map((data,id)=>
-    <>
-      <h3>{data.company}</h3>
-      <ul>
-      <strong>Jobs</strong>
-        <li  >
-          {data.jobs[0]}
-        </li>
-        <li >
-          {data.jobs[1]}
-        </li>
-      </ul>
-    </>
-    );
-    return(
-      <>
+function ArrOfObj() {
+  const listItem = list.map((data, id) =>
+    <li key={id} className="list-group-item">{data.name}</li>)
+  return (
+    <ul >{listItem}</ul>
+  );
+}
 
- {compObj}
- 
-      </>
-    );
-  }
-  
-  
-  
-  export { Hello, ObjProp, ArrayProp ,ArrOfObj,ComplexArrObj};
-  
+
+const complex = [{ company: 'XYZ', jobs: ['Javascript', 'React'] }, { company: 'ABC', jobs: ['AngularJs', 'Ionic'] }]
+function ComplexArrObj() {
+  const compObj = complex.map((data, id) =>
+    <>
+      <div className="card-body">
+        <h3 className="list-group-item">{data.company}</h3>
+        <ul>
+          <strong className="text-primary">Jobs</strong>
+          <li className="list-group-item">
+            {data.jobs[0]}
+          </li>
+          <li className="list-group-item">
+            {data.jobs[1]}
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+  return (
+    <>
+
+      {compObj}
+
+    </>
+  );
+}
+
+
+
+export { Hello, ObjProp, ArrayProp, ArrOfObj, ComplexArrObj };
